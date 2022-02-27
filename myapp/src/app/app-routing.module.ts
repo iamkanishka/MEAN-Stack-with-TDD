538b4690b48a93980fadfb2eaa1312933a8fd320
iamkanishka <kanishkabc123@gmail.com>
2022-02-27 23:42:54 +0530

    Added Dashboard Component

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import  { DashboardComponent} from '../app/dashboard/dashboard.component'
+const routes: Routes = [
+  { path: 'home', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
