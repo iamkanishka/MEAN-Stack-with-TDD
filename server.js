31f4ea3706a4946ba1f313a36fbd5f2bf283d599
iamkanishka <kanishkabc123@gmail.com>
2022-02-27 18:36:53 +0530

    Added dependencies

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const path = require("path");
 
 let app = express();
 
-var distDir = __dirname + "/myapp/dist/dockersample/index.html";
-app.use(express.static(distDir));
-app.use(express.static(path.join(__dirname, "/myapp/dist/dockersample/")));
+// var distDir = __dirname + "/myapp/dist/dockersample/index.html";
+// app.use(express.static(distDir));
+// app.use(express.static(path.join(__dirname, "/myapp/dist/dockersample/")));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -22,14 +22,21 @@ app.use(function (req, res, next) {
 
 app.use(cors());
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.get("/code", function (req, res) {
-    res.send('I love Coding');
-  });
+app.use(express.json({
+  limit: "50mb"
+}));
+app.use(express.urlencoded({
+  limit: "50mb",
+  extended: true
+}));
+// Put API functionality Routes below the route of Built Angular Application 
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/../dist/dockersample/index.html"));
+// });
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "/../dist/dockersample/index.html"));
+
+app.get("/code", function (req, res) {
+  res.send('I love Coding');
 });
 
 
