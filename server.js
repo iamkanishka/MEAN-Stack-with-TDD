6eb3ea666ee874ef5fef0599089aec075c646484
iamkanishka <kanishkabc123@gmail.com>
2022-02-27 18:47:01 +0530

    Added Database Connections

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require("express");
 var bodyparser = require("body-parser");
-
+var helmet = require("helmet");
 const cors = require("cors");
 const path = require("path");
-require('dotenv').config()
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+connectDB()
 
 let app = express();
 
-// var distDir = __dirname + "/myapp/dist/dockersample/index.html";
-// app.use(express.static(distDir));
-// app.use(express.static(path.join(__dirname, "/myapp/dist/dockersample/")));
+app.use(helmet());
 
+app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -20,27 +22,39 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(cors());
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+  })
+);
 
-app.use(express.json({
-  limit: "50mb"
-}));
-app.use(express.urlencoded({
-  limit: "50mb",
-  extended: true
-}));
-// Put API functionality Routes below the route of Built Angular Application 
+app.get("/", function (req, res) {
+  res.send("Helo World,I love Coding");
+});
+
+//server.close()
+app.listen(process.env.HOST_PORT, () => {
+  console.log("server listening on port " + process.env.HOST_PORT);
+});
+
+
+
+
+
+
+
+
+
+// var distDir = __dirname + "/myapp/dist/dockersample/index.html";
+// app.use(express.static(distDir));
+// app.use(express.static(path.join(__dirname, "/myapp/dist/dockersample/")));
+// Put API functionality Routes below the route of Built Angular Application
 // app.get("/*", function (req, res) {
 //   res.sendFile(path.join(__dirname, "/../dist/dockersample/index.html"));
 // });
-
-
-app.get("/code", function (req, res) {
-  res.send('I love Coding');
-});
-
-
-//server.close()
-app.listen(P, () => {
-  console.log("server listening on port " + PORT);
-});
